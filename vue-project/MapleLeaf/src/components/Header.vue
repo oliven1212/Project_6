61c671aa2606bf5506c2db90180248c6015f6fa1
Valdemar <pizza27@live.dk>
2024-10-24 14:59:36 +0200

    vkh

--- vue-project/MapleLeaf/src/components/Header.vue
+++ vue-project/MapleLeaf/src/components/Header.vue
@@ -4,9 +4,22 @@
 <template>
     <header> 
         <img src="" alt="">
-        <div>
-            <a href="./">Home</a>
-            <a href="./about">About</a>
+        <div id="headerItems">
+            <div>
+                <img src="./icons/homeIcon.png" alt="">
+                <a href="./"><h3>Home</h3></a>
+            </div>
+            <div>
+                <img src="./icons/homeIcon.png" alt="">
+                <a href="./about"><h3>Find apotek</h3></a>
+            </div>
+            <div>
+                <img src="./icons/homeIcon.png" alt="">
+                <a href="./"><h3>Kontakt os</h3></a>
+            </div>            <div>
+                <img src="./icons/homeIcon.png" alt="">
+                <a href="./"><h3>Log ind</h3></a>
+            </div>
         </div>
 
 
@@ -20,5 +33,22 @@
 header{
     background-color: #EEEEEE;
     height: 100px;
+}
+#headerItems {
+    display: flex;
+    flex-direction: row;
+}
+#headerItems div{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 100px;
+}
+#headerItems div a{
+    text-align: center
+}
+#headerItems div img{
+    display: block;
+    width: 50px;
 }
 </style>
