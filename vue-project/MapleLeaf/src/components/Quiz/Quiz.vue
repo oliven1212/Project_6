9db9205f4e1c78e12cd6634181537360b8c07989
valde <fe13valseh@gmail.com>
2024-10-29 13:27:54 +0100

    Quiz
    4 første side layout oprettelse

---
+++ vue-project/MapleLeaf/src/components/Quiz/Quiz.vue
@@ -0,0 +1,41 @@
+<script setup>
+</script>
+
+<template>
+    <div class='QuizMoreCHoice' >
+        <h1>Hvad vil du gerne have hjælp til?</h1>
+            <div>
+               <div><input type="checkbox" name="check-1" value="check-1" id="check-1" checked>
+      <label for="check-1">Choice 1</label></div>
+    <div><input type="checkbox" name="check-2" value="check-2" id="check-2">
+      <label for="check-2">Choice 2</label></div>
+    <div><input type="checkbox" name="check-3" value="check-3" id="check-3">
+      <label for="check-3">Choice 3</label></div>
+            </div>
+    </div>
+
+    <div class="QuizOneCHoice"  >
+        <div><input type="radio" name="choice" value="choice-1" id="choice-1" checked>
+      <label for="choice-1">Choice 1</label></div>
+    <div><input type="radio" name="choice" value="choice-2" id="choice-2">
+      <label for="choice-2">Choice 2</label></div>
+    <div><input type="radio" name="choice" value="choice-3" id="choice-3">
+      <label for="choice-3">Choice 3</label></div>
+    </div>
+    <div class="QuizYesNo"  >
+        <div><input type="radio" name="choice" value="choice-1" id="choice-1" checked>
+      <label for="choice-1">Choice 1</label></div>
+    <div><input type="radio" name="choice" value="choice-2" id="choice-2">
+      <label for="choice-2">Choice 2</label></div>
+    </div>
+    <div class="QuizString">
+    <div>
+        <input type=" text"/>
+    </div>
+    </div>
+</template>
+
+<style scoped>
+
+
+</style>
--- vue-project/MapleLeaf/src/views/AboutView.vue
+++ vue-project/MapleLeaf/src/views/AboutView.vue
@@ -1,9 +1,10 @@
 <script setup>
   import NavBar from '../components/Header.vue';
+  import Quiz from "../components/Quiz/Quiz.vue";
 </script>
 <template>
   <NavBar />
-
+<Quiz></Quiz>
 </template>
 
 <style>
