1fad3ca8db59bf4ee5892415ad65ee43873bf348
Valdemar <pizza27@live.dk>
2024-10-30 13:59:43 +0100

    Update LogIn.vue

--- vue-project/MapleLeaf/src/components/logIn/LogIn.vue
+++ vue-project/MapleLeaf/src/components/logIn/LogIn.vue
@@ -5,11 +5,27 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  
-};
 
+//gets the firebaseconfigs
+let secret;
+await fetch("https://projekt6-ebfa8-default-rtdb.europe-west1.firebasedatabase.app/secret.json",{
+    method: "GET"})
+        .then((response) => {
+            return response.json()
+        })
+        .then((result) => {
+            secret = result[Object.keys(result)];
+            
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = secret;
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
