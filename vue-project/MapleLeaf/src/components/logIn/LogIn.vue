b79021adc7c7bce07cd6f79b8e7a2e976791ea57
Valdemar <pizza27@live.dk>
2024-10-30 13:14:26 +0100

    Update LogIn.vue

--- vue-project/MapleLeaf/src/components/logIn/LogIn.vue
+++ vue-project/MapleLeaf/src/components/logIn/LogIn.vue
@@ -7,13 +7,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDMG5HfHDiIvuBMfnAROkBP1WBMpyzNr28",
-  authDomain: "projekt6-ebfa8.firebaseapp.com",
-  databaseURL: "https://projekt6-ebfa8-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "projekt6-ebfa8",
-  storageBucket: "projekt6-ebfa8.appspot.com",
-  messagingSenderId: "411594793378",
-  appId: "1:411594793378:web:b8b4609d3fb963134192fb"
+  
 };
 
 // Initialize Firebase
