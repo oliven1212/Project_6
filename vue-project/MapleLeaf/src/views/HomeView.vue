c344def0adac7d2eff36e68de08ca119b67566b1
Valdemar <pizza27@live.dk>
2024-10-30 13:06:20 +0100

    firebase start

--- vue-project/MapleLeaf/src/views/HomeView.vue
+++ vue-project/MapleLeaf/src/views/HomeView.vue
@@ -3,11 +3,13 @@ import NavBar from '../components/Header.vue';
 import FrontPageTop from '../components/FrontPageTop.vue';
 import Banner from '../components/Banner.vue';
 import Artikler from '../components/Artikler.vue';
+import LogIn from '@/components/logIn/LogIn.vue';
 </script>
 
 <template>
   <main>
     <NavBar />
+    <LogIn />
     <FrontPageTop />
     <Banner />
     <Artikler />
