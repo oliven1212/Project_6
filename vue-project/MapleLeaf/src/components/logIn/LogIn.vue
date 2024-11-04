ffaa5a43437cf709d987ae662a69ef23cd5258bd
Valdemar <pizza27@live.dk>
2024-11-04 14:37:22 +0100

    fuck vue

--- vue-project/MapleLeaf/src/components/logIn/LogIn.vue
+++ vue-project/MapleLeaf/src/components/logIn/LogIn.vue
@@ -1,41 +1,61 @@
 <script setup>
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth,createUserWithEmailAndPassword  } from "firebase/auth";
+import { ref } from 'vue';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
 //gets the firebaseconfigs
-let secret;
-await fetch("https://projekt6-ebfa8-default-rtdb.europe-west1.firebasedatabase.app/secret.json",{
+// Your web app's Firebase configuration
+let firebaseConfig = {};
+let auth;
+
+fetch("https://projekt6-ebfa8-default-rtdb.europe-west1.firebasedatabase.app/secret.json",{
     method: "GET"})
         .then((response) => {
             return response.json()
         })
         .then((result) => {
-            secret = result[Object.keys(result)];
-            
+            firebaseConfig = result[Object.keys(result)];
+        
+        // Initialize Firebase only after configuration is fetched
+        const app = initializeApp(firebaseConfig);
+        auth = getAuth(app);
+        
+        console.log("Firebase initialized successfully.");
         })
         .catch((error) => {
-            console.error(error);
+            console.error("Error fetching Firebase config or initializing Firebase:"+error);
         });
 
+let signUp = (email, password) => {
+let user;
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    // Signed up 
+    user = userCredential.user;
+    console.log(`${user.uid} is made`);
+    // ...
+    })
+    .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+});
+}
+let emailInput = ref("");
+let passwordInput = ref("");
 
-
-
-// Your web app's Firebase configuration
-const firebaseConfig = secret;
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
 
 </script>
 
 <template>
 <h1>aaaaaah</h1>
+<input type="text" v-model="emailInput" placeholder="type your Email">
+<input type="text" v-model="passwordInput" placeholder="type your Password">
+<button v-on:click="signUp(emailInput,passwordInput)">Join the cult!!!<br> we got cookies</button>
 </template>
 
 <style scoped>
--- vue-project/MapleLeaf/src/views/HomeView.vue
+++ vue-project/MapleLeaf/src/views/HomeView.vue
@@ -3,13 +3,15 @@ import NavBar from '../components/Header.vue';
 import FrontPageTop from '../components/FrontPageTop.vue';
 import Banner from '../components/Banner.vue';
 import Artikler from '../components/Artikler.vue';
-import LogIn from '@/components/logIn/LogIn.vue';
+import GoogleLogin from '../components/logIn/LogIn.vue';
+
 </script>
 
 <template>
   <main>
     <NavBar />
-    <LogIn />
+    <GoogleLogin />
+
     <FrontPageTop />
     <Banner />
     <Artikler />
