7313d20c13df7c9b032968c0205aaecfaafa1403
Valdemar <pizza27@live.dk>
2024-11-05 15:55:15 +0100

    sedt

--- vue-project/MapleLeaf/src/router/index.js
+++ vue-project/MapleLeaf/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AboutView from '../views/AboutView.vue';
+import LogInView from '../views/LogInView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,12 +12,14 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/Om-os',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: AboutView
+    },
+    {
+      path: '/LogInd',
+      name: 'LogIn',
+      component: LogInView
     }
   ]
 })
